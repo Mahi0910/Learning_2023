6a4d95112f3d888f134ed6636184bcb03ab268d7
MahithaMandapati <mahivarma7777@gmail.com>
2023-06-22 19:16:05 +0530

    commit 7 : main : Day7 Activities Added

--- Module1/Day7/parse_string.c
+++ Module1/Day7/parse_string.c
@@ -8,45 +8,69 @@ struct Student {
     float marks;
 };
 
-void parseStringToStructArray(const char *input, struct Student *students, int arraySize) {
-    char tempInput[100];
-    strcpy(tempInput, input);
-
-    for (int i = 0; i < arraySize; i++) {
-        // Get the rollno
-        char *token = strtok(tempInput, " ");
+void parseAndInitialize(struct Student* students, int numStudents) {
+    char inputString[100];
+    
+    for (int i = 0; i < numStudents; i++) {
+        printf("Enter details for Student %d:\n", i + 1);
+        
+        // Read the input string
+        printf("Input String: ");
+        fgets(inputString, sizeof(inputString), stdin);
+        
+        // Remove the newline character from the input string
+        inputString[strcspn(inputString, "\n")] = '\0';
+        
+        // Tokenize the input string
+        char* token = strtok(inputString, " ");
+        
+        // Parse and store the rollno
         students[i].rollno = atoi(token);
-        tempInput[strlen(token)] = '\0';
-
-        // Get the name
+        
+        // Get the next token (name)
         token = strtok(NULL, " ");
-        strcpy(students[i].name, token);
-        tempInput[strlen(token) + 1] = '\0';
-
-        // Get the marks
+        
+        // Parse and store the name
+        strncpy(students[i].name, token, sizeof(students[i].name) - 1);
+        students[i].name[sizeof(students[i].name) - 1] = '\0';
+        
+        // Get the next token (marks)
         token = strtok(NULL, " ");
+        
+        // Parse and store the marks
         students[i].marks = atof(token);
-        tempInput[strlen(token) + 1] = '\0';
+        
+        printf("\n");
+    }
+}
 
-        // Reset tempInput for the next iteration
-        strcpy(tempInput, &tempInput[strlen(token) + 1]);
+void printStudentData(struct Student* students, int numStudents) {
+    for (int i = 0; i < numStudents; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
     }
 }
 
 int main() {
-    const char inputString[] = "1001 Aron 100.00";
-    const int arraySize = 1;
-
-    struct Student *students = malloc(arraySize * sizeof(struct Student));
-
-    parseStringToStructArray(inputString, students, arraySize);
-
-    printf("Parsed Structure Array:\n");
-    for (int i = 0; i < arraySize; i++) {
-        printf("Student %d: Roll No - %d, Name - %s, Marks - %.2f\n",
-               i + 1, students[i].rollno, students[i].name, students[i].marks);
-    }
-
+    int numStudents;
+    printf("Enter the number of students: ");
+    scanf("%d", &numStudents);
+    getchar(); // Clear the newline character from the input buffer
+    
+    // Allocate memory for the array of structures
+    struct Student* students = (struct Student*)malloc(numStudents * sizeof(struct Student));
+    
+    // Read and initialize the data for each student
+    parseAndInitialize(students, numStudents);
+    
+    // Print the data for each student
+    printStudentData(students, numStudents);
+    
+    // Free the allocated memory
     free(students);
+    
     return 0;
 }
